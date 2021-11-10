a2662c9dc2d8fdfe3b89398a99a7b9358cb3e76a
Christopher <jokinha95@gmail.com>
2021-11-10 13:32:13 -0300

    Criado componentes

---
+++ src/components/Loja.js
@@ -0,0 +1,20 @@
+import React from "react"
+import styled from "styled-components";
+
+const ConteinerLoja= styled.div`
+`
+
+
+export default class Loja extends React.Component {
+
+
+
+    render() {
+       
+        return (
+            <ConteinerLoja>
+                Produtos da loja
+            </ConteinerLoja>
+        )
+    }
+}
