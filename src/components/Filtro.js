12562b2e0e3a6cda38a9b78d87ec032032bc5ba4
Igor Matsuoka <igor.mtka@gmail.com>
2021-11-11 17:51:48 -0300

    botao-ordem-finalizado

--- src/components/Filtro.js
+++ src/components/Filtro.js
@@ -1,12 +1,4 @@
 import React from "react"
-import styled from "styled-components";
-
-
-const ConteinerFiltro= styled.div`
-    border: 1px solid black;
-    width: 20vw;
-    height: 100vh;
-`
 
 export default class Filtro extends React.Component {
 
@@ -15,9 +7,9 @@ export default class Filtro extends React.Component {
     render() {
        
         return (
-            <ConteinerFiltro>
+            <div>
                 Filtro
-            </ConteinerFiltro>
+            </div>
         )
     }
 }
