40423ef33d49f2e844915559bf9d2dedb51be0ae
Gabriel Jorge <gabriel.ggcj@gmail.com>
2021-11-12 19:54:03 -0300

    arrumando carrinho

--- src/components/Carrinho.js
+++ src/components/Carrinho.js
@@ -1,19 +1,20 @@
-import React from "react"
-import styled from "styled-components";
+import React from 'react'
+import styled from 'styled-components'
 
+const ProdutoCarrinho = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+`;
 
-
-
-export default class Carrinho extends React.Component {
-
-
-
-    render() {
-       
-        return (
-            <div>
-                Carrinho
-            </div>
-        )
-    }
+export default function Carrinho(props) {
+    return (
+        <ProdutoCarrinho>
+            <p>{props.produto.quantidade}</p>
+            <p>{props.produto.produto.nome}</p>
+            <button value={props.produto.produto.id} onClick={props.funcao}>Remover</button>
+        </ProdutoCarrinho>
+    )
 }
